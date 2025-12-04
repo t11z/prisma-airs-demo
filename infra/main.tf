84f122d826ce6a36a445bc68ccae6b90b1b73047
Thomas Sprock <bettercallthomas@outlook.com>
2025-12-04 13:01:49 +0100

    bugfix

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_account" "storage" {
   https_traffic_only_enabled      = true
   min_tls_version                 = "TLS1_2"
   public_network_access_enabled   = false
-  shared_access_key_enabled       = true
+  shared_access_key_enabled       = false
   allow_nested_items_to_be_public = false
 
   tags = local.tags
